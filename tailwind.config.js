69e1769d0d6a85c47b1fc3c48fcd3089a5b790e6
Gary Basin <gbasin@gmail.com>
2024-12-31 14:35:40 -0800

    Tailwind fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/renderer/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      // TODO: Add custom colors, fonts, spacing, etc. as needed
-    },
+    extend: {},
   },
   plugins: [],
 } 
