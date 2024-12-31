be32db2c1ec3a0a1bea5161a7cbd5e9ddbfc7e93
Gary Basin <gbasin@gmail.com>
2024-12-31 14:32:31 -0800

    Adding css

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';  // Import our Tailwind CSS
 import App from './app';
 
 const container = document.getElementById('root');
